e33cc7b48bc2150c1f6dd8004bebb3d2752d5494
lusorious <lusorious@protonmail.com>
2020-06-17 18:24:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,274 @@
+const Discord = require('discord.js');
+const axios = require('axios');
+const bot = new Discord.Client();
+const Jimp = require('jimp')
+const map = require('./newmap')
+const mapping = map.mapping;
+
+
+bot.login('NjgwNTI1NjY3NTkwNDcxNzQ5.XuBonw.4NjIfz76OljwhSO996fPZoLK8F4');
+bot.on('message', msg => {
+    var msgContent = msg.content;
+    var usrCmd;
+    if (msgContent.startsWith('!')) {
+        usrCmd = msgContent.substring(1);
+    } else if (msgContent.toLowerCase().startsWith("pls ")) {
+        usrCmd = msgContent.substring(4);
+    } else {
+        return;
+    }
+
+    var cmd = mapping[usrCmd.split(" ")[0]];
+    if (cmd == null) {
+        return;
+    }
+
+    if (cmd.command == null || cmd.command == "sendFile") {
+        sendFile(msg, cmd);
+    } else if (cmd.command == 'protest') {
+        protest(msg, usrCmd);
+    } else if (cmd.command == "help") {
+        help(msg, usrCmd);
+    } else if (cmd.command == "faq") {
+        faq(msg);
+    } else if (cmd.command == "gulag") {
+        if (adminCheck(msg)) {
+            gulag(msg);
+        }
+    } else if (cmd.command == "ungulag") {
+        if (adminCheck(msg)) {
+            ungulag(msg);
+        }
+    } else if (cmd.command == "poll") {
+        if (adminCheck(msg)) {
+            poll(msg, usrCmd)
+        }
+    } else if (cmd.command == "rr") {
+        rr(msg);
+    }
+})
+
+function rr(msg) {
+    var i = getRandomInt(3);
+    if (i == 1) {
+        msg.reply("Bang! You will respawn from the gulag in 10 minutes");
+        doGulag(msg, msg.author);
+        setTimeout(() => {
+            doUngulag(msg, msg.author)
+        }, 600000);
+    } else {
+        msg.reply("Click! You live to shitpost another day");
+    }
+}
+
+function poll(msg, usrCmd) {
+    var split = usrCmd.split(", ");
+    var time = split[0].replace("poll ", "");
+    var title = split[1];
+    var i;
+    var answers = []
+    for (i = 2; i < split.length; i++) {
+        answers.push(split[i])
+    }
+
+    axios.post('https://strawpoll.com/api/poll', {
+        "poll": {
+            "title": title,
+            // "description": "GMD Poll",
+            "answers": answers,
+            "priv": true,
+            "ma": 0,
+            "mip": 0,
+            "co": 1,
+            "vpn": 0,
+            "enter_name": 0,
+            "has_deadline": false,
+            // "deadline": "2020-02-27T07:00:00.000Z",
+            "only_reg": 0,
+            "has_image": 0,
+            "image": null
+        }
+    })
+        .then(response => {
+            msg.channel.send("Poll created! Vote here: https://strawpoll.com/" + response.data.content_id);
+            setTimeout(() => {
+                axios.get("https://strawpoll.com/api/poll/" + response.data.content_id).then(res => {
+                    const exampleEmbed = {
+                        color: 0x0099ff,
+                        title: 'Poll Results',
+                        description: title,
+                        fields: [
+                        ]
+                    };
+                    res.data.content.poll.poll_answers.forEach(element => {
+                        exampleEmbed.fields.push({
+                            name: element.answer,
+                            value: element.votes + " votes"
+                        })
+                    });
+                    msg.reply({ embed: exampleEmbed });
+                }).catch(error => {
+                    console.log(error);
+                });
+            }, parseInt(time) * 60000)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+function gulag(msg) {
+    const user = msg.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+        doGulag(msg, user)
+        msg.reply(user + " has been sent to the gulag!");
+    }
+}
+
+function ungulag(msg) {
+    const user = msg.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+        doUngulag(msg, user);
+        msg.reply(user + " has been released from the gulag!");
+    }
+}
+
+function doUngulag(msg, user) {
+    // Now we get the member from the user
+    const member = msg.guild.member(user);
+    // If the member is in the guild
+    if (member) {
+        // Add the role!
+        let bad = msg.guild.roles.find(role => role.name === "Doad");
+        member.addRole(bad).catch(console.error);
+
+        // Remove a role!
+        let doad = msg.guild.roles.find(role => role.name === "Bad Dog");
+        member.removeRole(doad).catch(console.error);
+    }
+}
+
+function doGulag(msg, user) {
+    // Now we get the member from the user
+    const member = msg.guild.member(user);
+    // If the member is in the guild
+    if (member) {
+        // Add the role!
+        let bad = msg.guild.roles.find(role => role.name === "Bad Dog");
+        member.addRole(bad).catch(console.error);
+
+        // Remove a role!
+        let doad = msg.guild.roles.find(role => role.name === "Doad");
+        member.removeRole(doad).catch(console.error);
+    }
+}
+
+function adminCheck(msg) {
+    // get role by name
+    let myRole = msg.guild.roles.find(role => role.name === "Admin");
+    let isAdmin = msg.member.roles.has(myRole.id) || msg.author.id == '108309473692483584';
+    if (!isAdmin) {
+        msg.reply("Sorry, I can't do that for plebs!")
+    }
+
+    return isAdmin;
+}
+
+function faq(msg) {
+    const exampleEmbed = {
+        color: 0x0099ff,
+        title: 'GMD Bot FAQ',
+        description: "If you were based, you would know this stuff",
+        fields: [
+            {
+                name: "Why did my favorite channel disappear?",
+                value: "[Expand the Lounge dropdown](https://cdn.discordapp.com/attachments/674064859184365589/720574941325557810/image0.png)"
+            }
+        ],
+    };
+    msg.channel.send({ embed: exampleEmbed });
+}
+
+function help(msg, usrCmd) {
+    var i;
+    var k = 1;
+    var keys = Object.keys(mapping);
+
+    var split = usrCmd.split(" ");
+    if (split.length > 1) {
+        k = parseInt(split[1]);
+    }
+
+    const exampleEmbed = {
+        color: 0x0099ff,
+        title: 'GMD Bot Help',
+        description: "Type !command or pls command with any of the following:",
+        fields: [
+        ],
+        footer: {
+            text: 'Page ' + k + " of " + Math.ceil(keys.length / 24)
+        }
+    };
+
+    for (i = 0 + (24 * (k - 1)); i < 24 + (24 * (k - 1)); i++) {
+        if (keys[i] != null) {
+            exampleEmbed.fields.push({
+                name: keys[i],
+                value: mapping[keys[i]].desc,
+                inline: true
+            })
+        }
+    }
+
+    msg.channel.send({ embed: exampleEmbed });
+}
+
+function sendFile(msg, arr) {
+    if (arr != null) {
+        var file = arr.files[getRandomInt(arr.files.length)]
+        msg.channel.send("", {
+            files: [
+                "./files/" + file
+            ]
+        })
+    }
+}
+
+function protest(msg, name) {
+    name = name.replace('protest', "");
+    Jimp.read('img.png', (err, img) => {
+        var font;
+        if (err) throw err;
+        if (name.length > 110) {
+            msg.channel.send("Sorry " + msg.author + ", that's too long!");
+            return;
+        } else if (name.length > 40) {
+            font = Jimp.FONT_SANS_8_BLACK;
+        } else if (name.length > 25) {
+            font = Jimp.FONT_SANS_10_BLACK;
+        } else {
+            font = Jimp.FONT_SANS_16_BLACK;
+        }
+
+        Jimp.loadFont(font).then(font => {
+            img.print(font, 0, 5, {
+                text: name,
+                alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
+            }, 100, 20)
+                .write('test.png');
+            msg.channel.send("", {
+                files: [
+                    "./test.png"
+                ]
+            })
+        }).catch(err => console.log(err))
+
+
+    });
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
---
+++ newmap.js
@@ -0,0 +1,433 @@
+const mapping = {
+    "40": {
+        "command": "sendFile",
+        "files": ["4045.mp4"],
+        "desc": "40, 45 years?"
+    },
+    "aids": {
+        "files": ["aids.mp4"],
+        "desc": "Hot"
+    },
+    "ancient": {
+        "command": "sendFile",
+        "files": ["ancient.png", "ancient2.png"],
+        "desc": "thanks for the ancient news, idiot"
+    },
+
+    "aoc": {
+        "command": "sendFile",
+        "files": ["aoc.png"],
+        "desc": "AOC"
+    },
+    "aoc2": {
+        "command": "sendFile",
+        "files": ["aoc2.png"],
+        "desc": "AOC version 2"
+    },
+    "arti": {
+        "command": "sendFile",
+        "files": ["arti.mp4"],
+        "desc": "Jimmy's @Arti video"
+    }, 
+    "appreciate" : {
+        "files": ["appreciate.mp4"],
+        "desc": "I appriciate him"
+    },
+    "banter": {
+        "files": ["banter.mp4"],
+        "desc": "Banter"
+    },
+    "bebetter": {
+        "command": "sendFile",
+        "files": ["bebetter.mp4"],
+        "desc": "Be Better!"
+    },
+    "beef": {
+        "files": ["beef.mp4"],
+        "desc": "Hillary being thrown into van"
+    },
+    "beyond": {
+        "files": ["beyondangry.mp4"],
+        "desc": "I am beyond angry!"
+    },
+    "bitch": {
+        "files": ["bitch.mov"],
+        "desc": "Bitcn, are you for real?"
+    },
+    "boothisman": {
+        "files": ["boo.mp4"],
+        "desc": "Boo!"
+    },
+    "brexit": {
+        "files": ["boris_wall.gif"],
+        "desc": "Boris crashing through wall"
+    },
+    "cc": {
+        "files": ["cc.mp4"],
+        "desc": "Do you need my card?"
+    },
+    "cello": {
+        "files": ["cello.mp4"],
+        "desc": "cello, you’ve got a bass"
+    },
+    "censored": {
+        "files": ["censored.mov"],
+        "desc": "no fun allowed on reddit"
+    },
+    "chin": {
+        "files": ["chin1.gif", "chin2.mov"],
+        "desc": "Gavin looks like a freak"
+    },
+    "china": {
+        "files": ["china.mov"],
+        "desc": "Don't trust China!"
+    },
+    "cmon": {
+        "files": ["cmon.png"],
+        "desc": "c’mon, do something -> GMD"
+    },
+    "cocksucker": {
+        "files": ["cocksucker.mov"],
+        "desc": "Cocksucker"
+    },
+    "cool it": {
+        "files": ["Coolit.mp4"],
+        "desc": "cool it with the antisemitic remarks"
+    },
+    "crow": {
+        "files": ["crow_judge.png"],
+        "desc": "Crowof Judgement"
+    },
+    "damage": {
+        "files": ["damage.mp4"],
+        "desc": "Damage"
+    },
+    "depressed": {
+        "files": ["Depressed.jpg"],
+        "desc": "MichiganMan’s sad, stupid dog"
+    },
+    "doad": {
+        "files": ["doad1.mov", "doad2.mp4", "doad3.png"],
+        "desc": "I feel like I'm a doad"
+    },
+    "doanything": {
+        "files": ["DoesThisDoAnything.mp4"],
+        "desc": "This do anything?"
+    },
+    "doubt": {
+        "files": ["doubt.png"],
+        "desc": "(X) Doubt"
+    },
+    "eat": {
+        "files": ["eat.gif"],
+        "desc": "I will eat your ass!"
+    },
+    "fag": {
+        "files": ["Fag1.mov", "Fag2.mp4"],
+        "desc": "Communist faggot”, bully beatdown"
+    },
+    "fanny": {
+        "files": ["Fanny.mov"],
+        "desc": "unmute yourself ya fanny"
+    },
+    "faq": {
+        "command": "faq",
+        "desc": "Display the FAQ"
+    },
+    "freedom": {
+        "files": ["freedom.mov"],
+        "desc": "Jimmy reacts to brexit"
+    },
+    "fuckyou": {
+        "files": ["fuckyou.webm"],
+        "desc": "Hey, fuck you"
+    },
+    "ifeltgay": {
+        "files": ["feltgay.mp4"],
+        "desc": "I felt ugly, I felt gay"
+    },
+    "gae": {
+        "files": ["whyareyougay.mp4"],
+        "desc": "Why are you gae?"
+    },
+    "good": {
+        "files": ["good.mp4"],
+        "desc": "and being good at it if you will"
+    },
+    "gmdhelp": {
+        "command": "help",
+        "desc": "Display the help"
+    },
+    "gmdaz" : {
+        "files": ["gmdaz.png"],
+        "desc": "Greetings!"
+    },
+    "gulag" : {
+        "command": "gulag",
+        "desc": "Send them in"
+    },
+    "ungulag": {
+        "command": "ungulag",
+        "desc": "Break them out"
+    },
+    "hadenough": {
+        "files": ["hadenough.jpg"],
+        "desc": "delete server"
+    },
+    "hatethis": {
+        "files": ["HateThis.mov"],
+        "desc": "I hate this fuckin’ shit!"
+    },
+    "headout": {
+        "files": ["Headout1.gif", "Headout2.gif", "Headout3.png"],
+        "desc": "Headout"
+    },
+    "pimpin": {
+        "files": ["hespimpintrump.mp4"],
+        "desc": "I mean look at him, he’s pimpin"
+    },
+    "pimping": {
+        "files": ["hespimpintrump.mp4"],
+        "desc": "I mean look at him, he’s pimpin"
+    },
+    "poll": {
+        "command": "poll",
+        "desc": "Create a poll"
+    },
+    "howdareyou": {
+        "files": ["howdareyou.mov"],
+        "desc": "How dare you?"
+    },
+    "iamgay": {
+        "files": ["iamgay.mp4"],
+        "desc": "Here today because I'm gay"
+    },
+    "jesus": {
+        "files": ["jesus.gif"],
+        "desc": "Jesus"
+    },
+    "heels": {
+        "files": ["lmfywmho.mp4"],
+        "desc": "Heels On"
+    },
+    "leftwing": {
+        "files": ["leftwing.mov"],
+        "desc": "Left Wing"
+    },
+    "lucille": {
+        "files": ["lucille.gif"],
+        "desc": "Lucille"
+    },
+    "bullshit": {
+        "files": ["BS.mp4"],
+        "desc": "Bullshit!"
+    },
+    "memes": {
+        "files": ["memes.png"],
+        "desc": "We post memes here"
+    },
+    "message": {
+        "files": ["message.png"],
+        "desc": "They rejected His message"
+    },
+    "mikey": {
+        "files": ["Mikey.mov"],
+        "desc": "Mikey"
+    },
+    "mods": {
+        "files": ["mods.png"],
+        "desc": "Fuck the Mods"
+    },
+    "morning": {
+        "files": ["morning.png"],
+        "desc": "Good Morning"
+    },
+    "no": {
+        "files": ["no.jpg"],
+        "desc": "No!"
+    },
+    "nono": {
+        "files": ["nono.mp4"],
+        "desc": "Don't touch me there!"
+    },
+    "nussing": {
+        "files": ["NOTHING_WRONG.mp4"],
+        "desc": "Nothing wrong with that"
+    },
+    "oof": {
+        "files": ["oof.gif"],
+        "desc": "Oof level large"
+    },
+    "orgasm": {
+        "files": ["orgasm.mp4"],
+        "desc": "Orgasm"
+    },
+    "pathetic": {
+        "files": ["pathetic.png"],
+        "desc": "Pathetic"
+    },
+    "pelosi": {
+        "files": ["pelosi.jpg"],
+        "desc": "Pelosi"
+    },
+    "pepeschav": {
+        "files": ["pepeschav.png"],
+        "desc": "McGulag"
+    },
+    "pizza": {
+        "files": ["pizza.gif"],
+        "desc": "Trump Eating pizza"
+    },
+    "protest": {
+        "command": "protest",
+        "desc": "Create a sign for Pepe"
+    },
+    "ptsd": {
+        "files": ["ptsd1.mov", "ptsd2.webm", "ptsd3.mov"],
+        "desc": "PTSD"
+    },
+    "ptsd2": {
+        "files": ["ptsd2.webm"],
+        "desc": "PTSD version 2"
+    },
+    "racism": {
+        "files": ["racism.png"],
+        "desc": "GMD ends racism"
+    },
+    "removed": {
+        "files": [
+            "removed.mp4"], "desc":
+            "Banned from Reddit"
+    },
+    "riley": {
+        "files": ["riley.jpg"],
+        "desc": "Riley"
+    }, 
+    "rr" : {
+        "command": "rr",
+        "desc": "Russian Roulette"
+    },
+    "salute": {
+        "files": ["salute.gif"],
+        "desc": "Salute"
+    },
+    "schav": {
+        "files": ["schav.png"],
+        "desc": "Schav"
+    },
+    "see": {
+        "files": ["see-what-im-sayin.mp4"],
+        "desc": "See what I'm saying?"
+    },
+    "seethe": {
+        "files": ["seethe.gif"],
+        "desc": "Just Seethe"
+    },
+    "shittydiscord": {
+        "files": ["shittydiscord.mp4"],
+        "desc": "Thanks for the AMA on the shitty discord"
+    },
+    "shutup": {
+        "files": ["shutup.mov"],
+        "desc": "Shut up, bitch!"
+    },
+    "skedaddle": {
+        "files": ["skedaddle.jpg"],
+        "desc": "Time to skedaddle"
+    },
+    "spooky": {
+        "files": ["spookeeh.mp4"],
+        "desc": "Spoooooakkkkkkeeeaaaaahhhhnnnn"
+    },
+    "stop": {
+        "files": ["stop.mp4"],
+        "desc": "Holy music stops"
+    },
+    "stride": {
+        "files": ["stride.mp4"],
+        "desc": "Ain't nothing gonna break my stride"
+    },
+    "summon": {
+        "files": ["summon.png"],
+        "desc": "Whomst has summoned the almighty one?"
+    },
+    "supersoldier": {
+        "files": ["supersoldier.mp4"],
+        "desc": "Antifa Super Soldier"
+    },
+    "symbos": {
+        "files": ["symbos.mov"],
+        "desc": "Owdest Symbos"
+    },
+    "symbows": {
+        "files": ["symbos.mov"],
+        "desc": "Owdest Symbos"
+    },
+    "stella": {
+        "files": ["Stella.gif"],
+        "desc": "A Stella on Jimmy"
+    },
+    "simp": {
+        "files": ["simp.jpg"],
+        "desc": "Simp!"
+    },
+    "simp2": {
+        "files": ["simp2.webm"],
+        "desc": "Simp Video"
+    },
+    "takeover": {
+        "files": ["takeover.png"],
+        "desc": "Vins Take Over"
+    },
+    "talkingheads": {
+        "files": ["TalkingHeads.mp4"],
+        "desc": "Talking Heads/Trump mashup"
+    },
+    "true": {
+        "files": ["thatstrue.mp4"],
+        "desc": "That's true"
+    },
+    "trump": {
+        "files": ["Trump.png"],
+        "desc": "80's Trump"
+    },
+    "tucker": {
+        "files": ["Tucker.png"],
+        "desc": "Tucker Carlson"
+    },
+    "turnaround": {
+        "files": ["youturnaround2.mp4"],
+        "desc": "You turn around!"
+    },
+    "wall": {
+        "files": ["wall.png"],
+        "desc": "Pepe builds the wall"
+    },
+    "war": {
+        "command": "sendFile",
+        "files": ["war.mp4"],
+        "desc": "Andrew Breitbart 'War'"
+    },
+    "why": {
+        "files": ["why.mov"],
+        "desc": "Why are you gae?"
+    },
+    "woke": {
+        "files": ["woke.mov"],
+        "desc": "Woke"
+    },
+    "women": {
+        "files": ["women.mp4"],
+        "desc": "Defeating women"
+    },
+    "worthless": {
+        "files": ["worthless.mp4"],
+        "desc": "I felt worthless, I felt ugly, I felt gay"
+    },
+    "yikes": {
+        "files": ["yikes.png", "yikes2.png"],
+        "desc": "Trump total yikes moment"
+    }
+}
+
+exports.mapping = mapping;
